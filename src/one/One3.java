51bcca7d347623e400f5018edc18742b9f319bef
DESKTOP-K1FFKKF\sm917 <sm9171@nate.com>
2021-11-25 23:25:09 +0900

    Initial commit

---
+++ src/one/One3.java
@@ -0,0 +1,17 @@
+package one;
+
+public class One3 {
+    public static void main(String[] args) {
+        String a = "it is time to study";
+        String arr[] = a.split(" ");
+        String b = "";
+        int max =Integer.MIN_VALUE;
+        for (String s : arr) {
+            if(max<s.length()){
+                max=s.length();
+                b=s;
+            }
+        }
+        System.out.println(b);
+    }
+}
