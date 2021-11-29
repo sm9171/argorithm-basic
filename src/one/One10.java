c7c75aacdb32bf6fc6e4dc4e53e18a8edeebecee
Sangmin Na <>
2021-11-30 00:18:35 +0900

    20211129 - 가장 짧은 문자거리

--- src/one/One10.java
+++ src/one/One10.java
@@ -2,19 +2,35 @@ package one;
 
 public class One10 {
     public static void main(String[] args) {
-        String str = "teachermode e";
+        String str = "teachermode";
+        String str2 = "e";
+        char c = str2.charAt(0);
         One10 T = new One10();
-        System.out.println(T.Soloution(str));
+        for (int i : T.Soloution(str,c)) {
+            System.out.print(i+" ");
+        }
     }
-    private int Soloution(String str) {
-        String result="";
-        char c[] = str.toCharArray();
-
-        for (int i = 0; i < str.length(); i++) {
-            if (Character.isDigit(c[i])) {
-                result += c[i];
+    private int[] Soloution(String s, char t) {
+        int[] answer = new int[s.length()];
+        int p = 1000;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == t) {
+                p=0;
+                answer[i] = p;
+            }else{
+                p++;
+                answer[i] = p;
             }
         }
-        return Integer.parseInt(result);
+        p=1000;
+        for (int i = s.length()-1; i >=0; i--) {
+            if (s.charAt(i) == t) {
+                p=0;
+            }else{
+                p++;
+                answer[i] = Math.min(p,answer[i]);
+            }
+        }
+        return answer;
     }
 }
