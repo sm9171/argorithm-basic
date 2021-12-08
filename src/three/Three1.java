5202818302eca06081f166508271a5a8847a27cb
DESKTOP-K1FFKKF\sm917 <sm9171@nate.com>
2021-12-09 01:21:35 +0900

    20211208 - 두 배열 합치기, 공통원수 구하기

---
+++ src/three/Three1.java
@@ -0,0 +1,38 @@
+package three;
+
+
+import java.util.ArrayList;
+
+public class Three1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int[] arrA = {1, 3, 5};
+        int b = 5;
+        int[] arrB = {2, 3, 6, 7, 9};
+
+        Three1 T = new Three1();
+        for (int w :T.solution(a,arrA, b,arrB)) {
+            System.out.print(w+" ");
+        }
+
+    }
+    private ArrayList<Integer> solution(int a , int[] arrA, int b , int[] arrB) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int ai = 0, bi = 0;
+        while (ai < a && bi < b) {
+            if (arrA[ai] < arrB[bi]) {
+                result.add(arrA[ai++]);
+            } else {
+                result.add(arrB[bi++]);
+            }
+        }
+        while (ai < a) {
+            result.add(arrA[ai++]);
+        }
+        while (bi < b) {
+            result.add(arrB[bi++]);
+        }
+
+        return result;
+    }
+}
