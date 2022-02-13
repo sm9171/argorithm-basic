0e30d0f02bd59ae224bdd7183288434dd1b84642
sm9171 <sm9171@nate.com>
2022-02-14 01:41:47 +0900

    20220213 - 좌표 정렬

--- src/six/Six7.java
+++ src/six/Six7.java
@@ -1,25 +1,38 @@
 package six;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
+class Point implements Comparable<Point> {
+    public int x, y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.x == o.x) {
+            return this.y - o.y;
+        }else{
+            return this.x - o.x;
+        }
+    }
+}
 
 public class Six7 {
     public static void main(String[] args) {
         int n = 5;
-        int[][] arr = {
-                {2, 7},
-                {1, 3},
-                {1, 2},
-                {2, 5},
-                {3, 6}};
 
-        Six7 T = new Six7();
-        for (int x:T.solution(n, arr)) {
-            System.out.print(x+ " ");
-
+        ArrayList<Point> arr = new ArrayList<>();
+        arr.add(new Point(2, 7));
+        arr.add(new Point(1, 3));
+        arr.add(new Point(1, 2));
+        arr.add(new Point(2, 5));
+        arr.add(new Point(3, 6));
+        Collections.sort(arr);
+        for (Point point:arr) {
+            System.out.println(point.x + " " + point.y);
         }
-    }
-    private ArrayList<Integer> solution(int n, int[][] arr) {
-        ArrayList<Integer> result = new ArrayList<>();
-        return result;
     }
 }
