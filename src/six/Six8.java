bf7e41232b2f394d09146fc4f12b6f8b269ec781
sm9171 <sm9171@nate.com>
2022-03-26 16:43:24 +0900

    20220326 - 이분검색

---
+++ src/six/Six8.java
@@ -0,0 +1,34 @@
+package six;
+
+import java.util.Arrays;
+
+public class Six8 {
+    public static void main(String[] args) {
+        int n = 8;
+        int m = 32;
+        int[] arr = {23, 87, 65, 12, 57, 32, 99, 81};
+
+        Six8 T = new Six8();
+        System.out.println( T.solution(n,m,arr));
+    }
+
+    private int solution(int n, int m, int[] arr) {
+        int result = 0;
+        Arrays.sort(arr);
+        int lt= 0;
+        int rt = n - 1;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (arr[mid] == m) {
+                result = mid + 1;
+                break;
+            }
+            if (arr[mid] > m) {
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        return result;
+    }
+}
