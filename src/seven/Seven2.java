021a4fe58871563eb70ca9bf86380134bd2abba3
sm9171 <sm9171@nate.com>
2022-03-30 00:22:09 +0900

    feature(7-2): 이진수 출력(재귀)

---
+++ src/seven/Seven2.java
@@ -0,0 +1,17 @@
+package seven;
+
+public class Seven2 {
+    public static void main(String[] args) {
+        int n = 11;
+        Seven2 T = new Seven2();
+        T.DFS(n);
+    }
+    private void DFS(int n) {
+        if (n == 0) {
+            return;
+        }else{
+            System.out.println(n%2);
+            DFS(n/2);
+        }
+    }
+}
