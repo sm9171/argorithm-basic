b0cdcd82d529c715da859ff9d54454e05e16ce13
Sangmin <sm9171@nate.com>
2022-04-06 01:05:02 +0900

    feat(7-10): TREE 말단노드까지의 가장 짧은 경로(BFS)

---
+++ src/seven/Seven10.java
@@ -0,0 +1,41 @@
+package seven;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Seven10 {
+    Node root;
+
+    public int BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0;
+        while (!Q.isEmpty()) {
+            int size = Q.size();
+            for (int i = 0; i < size; i++) {
+                Node cur = Q.poll();
+                if (cur.lt == null && cur.rt == null) {
+                    return L;
+                }
+                if (cur.lt != null) {
+                    Q.offer(cur.lt);
+                }
+                if (cur.rt != null) {
+                    Q.offer(cur.rt);
+                }
+            }
+            L++;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        Seven10 tree = new Seven10();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(tree.BFS(tree.root));
+    }
+}
