634a2f973ba5c46a1f85ba7656adad63d8ae5ce4
Sangmin <sm9171@nate.com>
2022-08-07 21:10:31 +0900

    백준 2775문제

--- src/solved/class2/bak_2775.java
+++ src/solved/class2/bak_2775.java
@@ -1,4 +1,40 @@
 package solved.class2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class bak_2775 {
+    public static int[][] APT = new int[15][15];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        make_APT();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            sb.append(APT[k][n]).append('\n');
+        }
+        System.out.println(sb);
+        br.close();
+    }
+
+    public static void make_APT() {
+        // 아파트 생성
+
+        for (int i = 0; i < 15; i++) {
+            APT[i][1] = 1; // i층 1호
+            APT[0][i] = i; // 0층 i호
+        }
+
+        for (int i = 1; i < 15; i++) { // 1층부터 14층까지
+
+            for (int j = 2; j < 15; j++) { // 2호부터 14호까지
+                APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+            }
+        }
+    }
 }
