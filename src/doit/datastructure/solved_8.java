5eb5e68dfbafc973ff5597887c3bbd80d0252cb9
Sangmin <sm9171@nate.com>
2022-08-28 19:43:55 +0900

    doit 문제 8

---
+++ src/doit/datastructure/solved_8.java
@@ -0,0 +1,47 @@
+package doit.datastructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class solved_8 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long arr[] = new long[N];
+        int result = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            long find = arr[i];
+            int lt = 0;
+            int rt = N - 1;
+
+            while (lt < rt) {
+                if (arr[lt] + arr[rt] == find) {
+                    if (lt != i && rt != i) {
+                        result++;
+                        break;
+                    } else if (lt == i) {
+                        lt++;
+                    } else if (rt == i) {
+                        rt--;
+                    }
+                } else if (arr[lt] + arr[rt] < find) {
+                    lt++;
+                } else {
+                    rt--;
+                }
+            }
+        }
+        System.out.println(result);
+        br.close();
+    }
+}
