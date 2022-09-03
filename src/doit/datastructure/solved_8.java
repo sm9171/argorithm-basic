0d012ca652397837f6374deec62a703dd97a8189
Sangmin <sm9171@nate.com>
2022-09-04 01:12:34 +0900

    doit 문제 9

--- src/doit/datastructure/solved_8.java
+++ src/doit/datastructure/solved_8.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+//bak_1253
 public class solved_8 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
