b5b13ac90b1b905103439efb3886502525279333
Sangmin <sm9171@nate.com>
2022-09-07 01:51:30 +0900

    doit 문제 11

--- src/doit/datastructure/solved_11.java
+++ src/doit/datastructure/solved_11.java
@@ -1,12 +1,42 @@
 package doit.datastructure;
 
-import java.io.*;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+//bak_1847
 public class solved_11 {
     public static void main(String[] args) throws IOException {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int A[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer sb = new StringBuffer();
+        int num = 1;
+        boolean result = true;
+        for (int i = 0; i < A.length; i++) {
+            int su = A[i];
+            if (su >= num) {
+                while (su >= num) {
+                    stack.push(num++);
+                    sb.append("+\n");
+                }
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                int n = stack.pop();
+                if (su < n) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                }else{
+                    sb.append("-\n");
+                }
+            }
+        }
+        if(result) System.out.println(sb.toString());
     }
 }
