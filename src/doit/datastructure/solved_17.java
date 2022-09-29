27ac45989929245ce06fcf078a7646e4788b9b32
Sangmin <sm9171@nate.com>
2022-09-29 23:05:31 +0900

    doit 문제 17

--- src/doit/datastructure/solved_17.java
+++ src/doit/datastructure/solved_17.java
@@ -1,4 +1,33 @@
 package doit.datastructure;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//boj_1427
 public class solved_17 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int A[] = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            A[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+        for (int i = 0; i < str.length(); i++) {
+            int Max = i;
+            for (int j = i + 1; j < str.length(); j++) {
+                if (A[j] > A[Max]) {
+                    Max = j;
+                }
+            }
+            if (A[i] < A[Max]) {
+                int temp = A[i];
+                A[i] = A[Max];
+                A[Max] = temp;
+            }
+        }
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(A[i]);
+        }
+    }
 }
