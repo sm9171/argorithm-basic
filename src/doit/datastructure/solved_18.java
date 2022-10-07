bd185485acfcb13bd04ded44a5ad01f32d3e593d
Sangmin <sm9171@nate.com>
2022-10-07 22:36:23 +0900

    doit 문제 18

--- src/doit/datastructure/solved_18.java
+++ src/doit/datastructure/solved_18.java
@@ -1,5 +1,19 @@
 package doit.datastructure;
 
-public class solved_18 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class solved_18 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+    }
 }
