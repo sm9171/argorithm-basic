b865791c483b8f034c8aed81b12341ff9c8a9615
Sangmin <sm9171@nate.com>
2022-11-10 23:53:53 +0900

    doit 문제 24

--- src/doit/datastructure/solved_24.java
+++ src/doit/datastructure/solved_24.java
@@ -1,4 +1,40 @@
 package doit.datastructure;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class solved_24 {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+    }
+
+    private static void DFS(int number, int jarisu) {
+        if (jarisu == N) {
+            if (isPrime(number)) {
+                System.out.println(number);
+            }
+            return;
+        }
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if (isPrime(number * 10 + i)) {
+                DFS(number * 10 + i, jarisu + 1);
+            }
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
