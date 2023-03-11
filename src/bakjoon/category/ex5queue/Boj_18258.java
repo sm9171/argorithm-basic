4d71dbfb638b7316e07f2d61d3fbddbc38695c37
Sangmin <sm9171@nate.com>
2023-03-11 11:40:26 +0900

    feat(queue): 2164,10845,18258 문제풀이

---
+++ src/bakjoon/category/ex5queue/Boj_18258.java
@@ -0,0 +1,87 @@
+package bakjoon.category.ex5queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_18258 {
+    private static int[] queue = new int[2000001];
+    private static int first = 0;
+    private static int last = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "push" :
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop" :
+                    sb.append(pop()).append("\n");
+                    break;
+                case "size" :
+                    sb.append(size()).append("\n");
+                    break;
+                case "empty" :
+                    sb.append(empty()).append("\n");
+                    break;
+                case "front" :
+                    sb.append(front()).append("\n");
+                    break;
+                case "back" :
+                    sb.append(back()).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static void push(int x) {
+        queue[last] = x;
+        last++;
+    }
+
+    private static int pop() {
+        if (last - first == 0) {
+            return -1;
+        } else {
+            int num = queue[first];
+            first++;
+            return num;
+        }
+    }
+
+    private static int size() {
+        return last - first;
+    }
+
+    private static int empty() {
+        if(last - first == 0) {
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+
+    private static int front() {
+        if (last - first == 0) {
+            return -1;
+        } else {
+            return queue[first];
+        }
+    }
+
+    private static int back() {
+        if (last - first == 0) {
+            return -1;
+        } else {
+            return queue[last - 1];
+        }
+    }
+}
---
+++ src/bakjoon/category/ex5queue/Boj_2164.java
@@ -0,0 +1,23 @@
+package bakjoon.category.ex5queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Boj_2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue queue = new LinkedList();
+        for (int i = 0; i < N; i++) {
+            queue.add(i + 1);
+        }
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}
