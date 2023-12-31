310fc9d92da79726fb5eed4bbf30d9f02a3172b7
Sangmin <sm9171@nate.com>
2023-12-31 22:36:15 +0900

    feat: 2231 문제

---
+++ src/study/week5/boj_2231.java
@@ -0,0 +1,27 @@
+package study.week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int result = 0;
+        for (int i = 1; i < N; i++) {
+            int sum = 0;
+            int temp = i;
+            while (temp != 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            if (sum + i == N) {
+                result = i;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
